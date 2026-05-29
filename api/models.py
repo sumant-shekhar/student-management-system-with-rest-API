609ff303033be3f4c147afcc91e1bbec8510247c
Sumant-Shekhar <sumantshekhar.dev@gmail.com>
2026-05-29 20:45:40 +0530

    setup model for database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    student_class = models.CharField(max_length=20)
+    father_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
