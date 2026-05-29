65358bbb3cae21aada0ce7e1bb222b24de7e9fa2
Sumant-Shekhar <sumantshekhar.dev@gmail.com>
2026-05-29 20:54:21 +0530

    register Student model in admin and define admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'admission_number',
+        'first_name',
+        'last_name',
+        'student_class',
+        'date_of_birth',
+        'email',
+        )
+
+    search_fields = (
+        'admission_number',
+        'first_name',
+        'last_name',
+        'email',
+        )
+
+    list_filter = ('student_class',)
